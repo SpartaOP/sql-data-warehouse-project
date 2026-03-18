a37b20b1694abfbda0a0792869480f4d987febad
SpartaOP <69449542+SpartaOP@users.noreply.github.com>
2026-03-18 23:29:02 +0530

    Insert clean data into silver schema tables
    Inserts clean data into silver schema tables for customer info, product info, and sales details with data standardization and null handling.

---
+++ scripts/silver-clean-insert.sql
@@ -0,0 +1,100 @@
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr
+) -- Inserting clean data into silver schema table
+
+SELECT 
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname, -- Eliminating extra spaces
+	TRIM(cst_lastname) AS cst_lastname, -- Eliminating extra spaces
+		CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+		 WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		 ELSE 'na' -- Data Standardization
+	END AS cst_marital_status,
+	CASE WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+		 WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		 ELSE 'na' -- Data Standardization
+	END AS cst_gndr
+FROM (
+	SELECT 
+	*, ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1; -- Eliminating dpulicate primary key (customer id in this case)
+
+
+-- =======================================================
+
+
+INSERT INTO silver.crm_prd_info (
+	prd_id, 
+	cat_id, 
+	prd_key, 
+	prd_nm, 
+	prd_cost, 
+	prd_line, 
+	prd_start_dt, 
+	prd_end_dt
+) -- Inserting clean data into silver schema table
+
+SELECT
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extracting category id from product key and replacing '-' with '_' for joining purposes
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- Extracting remaining substring from product key
+	TRIM(prd_nm) AS prd_nm, -- Eliminate extra spaces
+	ISNULL(prd_cost,0) AS prd_cost, -- Null handling
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'na' -- Data Standardization
+	END AS prd_line,
+	CAST(prd_start_dt AS DATE) AS prd_start_dt, -- Typecasting to DATE as no TIME provided
+	CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt -- Calculating end date as one day before the next start date for the same product key
+FROM bronze.crm_prd_info
+
+
+-- =======================================================
+
+
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+) -- Inserting clean data into silver schema table
+
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) !=8 THEN NULL
+		 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt, -- Typecasting to DATE and handling invalid date formats
+	CASE WHEN sls_order_dt = 0 OR LEN(sls_ship_dt) !=8 THEN NULL
+		 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt, -- Typecasting to DATE and handling invalid date formats
+	CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) !=8 THEN NULL
+		 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt, -- Typecasting to DATE and handling invalid date formats
+	CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+		 THEN sls_quantity * ABS(sls_price)
+		 ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE WHEN sls_price <= 0 OR sls_price IS NULL
+		 THEN sls_sales / NULLIF(sls_quantity, 0)
+		 ELSE sls_price
+	END AS sls_price
+FROM bronze.crm_sales_details
