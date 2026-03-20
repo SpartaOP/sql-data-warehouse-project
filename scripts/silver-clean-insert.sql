9545b74f5b20bce1d06e87bf213841e5335f6841
SpartaOP <69449542+SpartaOP@users.noreply.github.com>
2026-03-20 21:25:20 +0530

    Update silver-clean-insert.sql

--- scripts/silver-clean-insert.sql
+++ scripts/silver-clean-insert.sql
@@ -141,3 +141,9 @@ FROM bronze.erp_loc_a101 -- Data Standardization
 -- ========================================================
 
 
+INSERT INTO silver.erp_px_cat_g1v2(id, cat, subcat, maintenance)
+
+SELECT id, cat, subcat, maintenance FROM bronze.erp_px_cat_g1v2; -- Alrady Clean Data
+
+
+-- =======================================================
